b70fd6ef02a08b0248e1fec77b497788d276c119
decobot <capy@deco.cx>
2024-12-16 23:14:41 -0300

    ingresso

---
+++ src/main/java/SistemaIngressos/Ingresso.java
@@ -0,0 +1,37 @@
+public class Ingresso {
+    private final int id;
+    private final TipoIngresso tipo;
+    private StatusIngresso status;
+    private final double preco;
+
+    public Ingresso(int id, TipoIngresso tipo, double preco) {
+        this.id = id;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.status = StatusIngresso.DISPONIVEL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public TipoIngresso getTipo() {
+        return tipo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public StatusIngresso getStatus() {
+        return status;
+    }
+
+    public void marcarComoVendido() {
+        this.status = StatusIngresso.VENDIDO;
+    }
+
+    public boolean isVendido() {
+        return this.status == StatusIngresso.VENDIDO;
+    }
+}
